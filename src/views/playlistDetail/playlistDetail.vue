894d7c6427c0c5b3d6bf5a148b3b4f375409eb0f
叶子 <xiaoyexiang@126.com>
2019-04-03 14:07:17 +0800

    test
    test

--- src/views/playlistDetail/playlistDetail.vue
+++ src/views/playlistDetail/playlistDetail.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="list-container">
-        <header>
-            <img width="200" height="200" v-lazy="playlist.coverImgUrl" alt="">
-        </header>
-        <ol>
-            <li :key="index" v-for="(song, index) in playlist.tracks" @click="goPlay(song.id)">
-                <h2 v-html="song.name"></h2>
-            </li>
-        </ol>
+        <!-- <div class="cover-zone">
+            <img class="play-detail-cover" width="200" height="200" v-lazy="playlist.coverImgUrl" alt="">
+        </div> -->
+        <ul>
+            <template v-for="(song, index) in playlist.tracks">
+                <li class="songlist" :data-song-id="song.id" @click.prevent.stop="goPlay(song.id)">
+                    <p v-html="song.name"></p>
+                </li>
+            </template>
+        </ul>
     </div>
 </template>
 <script>
@@ -50,8 +52,8 @@ export default {
         if(!this.playlistId) {
             this.goBack();
         }
-        // this.playlistId = this.$route.params.id;
-        this.playlistId = 19723756;
+        this.playlistId = this.$route.params.id;
+        // this.playlistId = 19723756;
         console.log(this.playlistId);
         this.getPlaylistDetail();
     },
@@ -59,5 +61,28 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
-
+    .cover-zone {
+        position: relative;
+        display: block;
+        width: 200px;
+        height: 200px;
+        margin: 20px auto;
+        .play-detail-cover {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0
+            bottom: 0
+            margin: auto
+            display: block;
+            width: 200px;
+            height: 200px;
+        }
+    }
+    .songlist {
+        display: block
+        padding: 0 10px
+        height: 40px;
+        line-height: 40px
+    }
 </style>
