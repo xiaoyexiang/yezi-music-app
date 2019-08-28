4ab80984681daca821338f98e430c611324539b3
叶子 <xiaoyexiang@126.com>
2019-08-28 10:23:44 +0800

    prod.server.js

--- src/javascript/requestServer.js
+++ src/javascript/requestServer.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-let baseURL = process.env.NODE_ENV === 'production' ? 'http://www.xiaoyexiang.com:3000' : 'http://localhost:4000';
+let baseURL = process.env.NODE_ENV === 'production' ? 'http://www.xiaoyexiang.com/api_music' : 'http://localhost:4000';
+if(window.location.protocol === 'https:') {
+  baseURL = replace('http:', 'https:');
+}
 console.log('baseURL: ', baseURL);
 
 const requestServer = axios.create({
